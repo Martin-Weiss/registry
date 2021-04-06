961f513b65f42b2f4c281cd5625d1294f6b36ee7
root <martin.weiss@suse.com>
2021-04-06 13:00:11 +0200

    added rke2 1.20.5

--- staging-rke/get-image-lists.sh
+++ staging-rke/get-image-lists.sh
@@ -5,6 +5,7 @@
 #Get Image Text files
 wget -N https://github.com/rancher/rke2/releases/download/v1.19.7%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.19.7+rke2r1.txt
 wget -N https://github.com/rancher/rke2/releases/download/v1.20.4%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.20.4+rke2r1.txt
+wget -N https://github.com/rancher/rke2/releases/download/v1.20.5%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.20.5+rke2r1.txt
 
 # Rancher
 wget -N https://github.com/rancher/rancher/releases/download/v2.5.5/rancher-images.txt -O rancher-images-v2.5.5.txt; sed -i "s#^#docker.io/#g" rancher-images-v2.5.5.txt
