a33e1ed70fdc29592f762e73fc4c2e9d3c3d8696
root <martin.weiss@suse.com>
2021-04-13 13:46:38 +0200

    testing v1.20.5-alpha1+rke2r2.txt

--- staging-rke/get-image-lists.sh
+++ staging-rke/get-image-lists.sh
@@ -2,11 +2,15 @@
 # Get Docker-ls
 #wget -N https://github.com/mayflower/docker-ls/releases/download/v0.5.1/docker-ls-linux-amd64.zip
 
-#Get Image Text files
-wget -N https://github.com/rancher/rke2/releases/download/v1.19.7%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.19.7+rke2r1.txt
-wget -N https://github.com/rancher/rke2/releases/download/v1.20.4%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.20.4+rke2r1.txt
-wget -N https://github.com/rancher/rke2/releases/download/v1.20.5%2Brke2r1/rke2-images.linux-amd64.txt -O rke2-images.linux-amd64-v1.20.5+rke2r1.txt
+RKE2_VERSIONS="v1.19.7+rke2r1 v1.20.4+rke2r1 v1.20.5+rke2r1 v1.20.5-alpha1+rke2r2"
+RANCHER_VERSIONS="v2.5.5 v2.5.7"
+
+# RKE2
+for RKE2_VERSION in $RKE2_VERSIONS; do
+	wget -N "https://github.com/rancher/rke2/releases/download/$RKE2_VERSION/rke2-images.linux-amd64.txt" -O rke2-images.linux-amd64-$RKE2_VERSION.txt
+done
 
 # Rancher
-wget -N https://github.com/rancher/rancher/releases/download/v2.5.5/rancher-images.txt -O rancher-images-v2.5.5.txt; sed -i "s#^#docker.io/#g" rancher-images-v2.5.5.txt
-wget -N https://github.com/rancher/rancher/releases/download/v2.5.7/rancher-images.txt -O rancher-images-v2.5.7.txt; sed -i "s#^#docker.io/#g" rancher-images-v2.5.7.txt
+for RANCHER_VERSION in $RANCHER_VERSIONS; do
+	wget -N "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt" -O rancher-images-$RANCHER_VERSION.txt; sed -i "s#^#docker.io/#g" rancher-images-$RANCHER_VERSION.txt
+done
