1b343dc7be4ac930c8460f8369a151bc25bf0b5f
root <martin.weiss@suse.com>
2021-04-22 13:50:49 +0200

    adding latest rke2 versions

--- staging-rke/get-image-lists.sh
+++ staging-rke/get-image-lists.sh
@@ -2,7 +2,7 @@
 # Get Docker-ls
 #wget -N https://github.com/mayflower/docker-ls/releases/download/v0.5.1/docker-ls-linux-amd64.zip
 
-RKE2_VERSIONS="v1.19.7+rke2r1 v1.19.8+rke2r1 v1.19.9+rke2r1 v1.20.4+rke2r1 v1.20.5+rke2r1 v1.20.5-alpha1+rke2r2 v1.20.6-rc2+rke2r1"
+RKE2_VERSIONS="v1.19.7+rke2r1 v1.19.8+rke2r1 v1.19.9+rke2r1 v1.19.10+rke2r1 v1.20.4+rke2r1 v1.20.5+rke2r1 v1.20.6+rke2r1"
 RANCHER_VERSIONS="v2.5.5 v2.5.7"
 
 # RKE2
