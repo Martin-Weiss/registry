a0664a66deb7631c6d45f3e9acad7572211df3d8
root <martin.weiss@suse.com>
2021-05-20 11:35:10 +0200

    enable gitlab container registry

--- gitlab/gitlab.sh
+++ gitlab/gitlab.sh
@@ -23,14 +23,12 @@ $RUNTIME run -d \
 -v /data/certificates/$KEY:/etc/gitlab/ssl/$FQDN.key:ro \
 -p 10080:80 \
 -p 10443:10443 \
+-p 15000:15000 \
 -p 10022:22 \
--e GITLAB_OMNIBUS_CONFIG="external_url 'https://$FQDN:10443/'; letsencrypt['enable'] = false; nginx['redirect_http_to_https'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 10022;" \
+-e GITLAB_OMNIBUS_CONFIG="external_url 'https://$FQDN:10443/'; letsencrypt['enable'] = false; nginx['redirect_http_to_https'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 10022; registry_external_url 'https://$FQDN:15000'; gitlab_rails['registry_path'] = '/var/opt/gitlab/gitlab-rails/shared/registry'; gitlab_rails['gitlab_default_projects_features_container_registry'] = true; registry['enable'] = true; registry_nginx['enable'] = true; registry_nginx['ssl_certificate'] = '/etc/gitlab/ssl/$FQDN.crt'; registry_nginx['ssl_certificate_key'] = '/etc/gitlab/ssl/$FQDN.key' " \
 docker.io/gitlab/gitlab-ce:13.11.4-ce.0
 podman generate systemd --restart-policy=always gitlab -n > /etc/systemd/system/container-gitlab.service
 systemctl daemon-reload
 systemctl enable --now container-gitlab.service
 exit
--p 10443:10443 \
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.example.com.crt"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.example.com.key"
--v /data/certificates/$CA_CERTIFICATE:/etc/gitlab/trusted-certs/ca-bundle.pem:ro \
+-e GITLAB_OMNIBUS_CONFIG="external_url 'https://$FQDN:10443/'; letsencrypt['enable'] = false; nginx['redirect_http_to_https'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 10022;" \
