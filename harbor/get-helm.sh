f222ae12d739040aa654f3e360ea85b47904f43e
Martin-Weiss <martin.weiss@suse.com>
2022-04-13 08:43:22 +0200

    updating k3s and harbor

---
+++ harbor/get-helm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -L -o helm-linux-amd64.tar.gz https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
+tar xzf helm-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin
+rm -rf linux-amd64
+rm helm-linux-amd64.tar.gz
+/usr/local/bin/helm completion bash > /usr/share/bash-completion/completions/helm
+chmod 644 /usr/share/bash-completion/completions/helm
--- harbor/get-images.sh
+++ harbor/get-images.sh
@@ -2,13 +2,13 @@
 source variables.txt
 mkdir -p harbor-$HARBOR_CHART_VERSION/images
 echo "images" > harbor-$HARBOR_CHART_VERSION/.gitignore
-for IMAGE in $(cat harbor-images-$HARBOR_VERSION.txt); do
+for IMAGE in $(cat harbor-images-v$HARBOR_VERSION.txt); do
 	echo $IMAGE
 	SHORTIMAGE=$(echo $IMAGE|awk -F '/' '{print $NF}')
 	IMAGE_FILENAME=$(echo $SHORTIMAGE.tar|sed s#:#X#g)
-#	skopeo copy docker://$IMAGE docker-archive:harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
-	docker pull $IMAGE
-	docker save $IMAGE -o harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
-	docker rmi $IMAGE
+	skopeo copy docker://$IMAGE docker-archive:harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
+#	docker pull $IMAGE
+#	docker save $IMAGE -o harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
+#	docker rmi $IMAGE
 done
 
