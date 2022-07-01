adf63e10af1e91f2dacb4fb012224e2da3ff94ca
root <martin.weiss@suse.com>
2022-07-01 11:48:37 +0200

    adding rancher 2.6.6

--- staging-rke/get-image-lists.sh
+++ staging-rke/get-image-lists.sh
@@ -3,7 +3,7 @@
 #wget -N https://github.com/mayflower/docker-ls/releases/download/v0.5.1/docker-ls-linux-amd64.zip
 
 RKE2_VERSIONS="v1.19.7+rke2r1 v1.19.8+rke2r1 v1.19.9+rke2r1 v1.19.10+rke2r1 v1.19.11+rke2r1 v1.19.12+rke2r1 v1.19.13+rke2r1 v1.19.14+rke2r1 v1.19.16+rke2r1 v1.20.4+rke2r1 v1.20.5+rke2r1 v1.20.6+rke2r1 v1.20.7+rke2r1 v1.20.7+rke2r2 v1.20.8+rke2r1 v1.20.9+rke2r1 v1.20.10+rke2r1 v1.20.11+rke2r1 v1.20.11+rke2r2 v1.20.12+rke2r1 v1.20.13+rke2r1 v1.20.15+rke2r1 v1.21.2+rke2r1 v1.21.3+rke2r1 v1.21.4+rke2r3 v1.21.5+rke2r1 v1.21.6+rke2r1 v1.21.7+rke2r1 v1.21.7+rke2r2 v1.21.9+rke2r1 v1.21.10+rke2r1 v1.21.11+rke2r1 v1.22.8+rke2r1 v1.22.9+rke2r2"
-RANCHER_VERSIONS="v2.5.5 v2.5.7 v2.5.8 v2.5.9 v2.5.11 v2.6.0 v2.6.1 v2.6.2 v2.6.3 v2.6.4 v2.6.5"
+RANCHER_VERSIONS="v2.5.5 v2.5.7 v2.5.8 v2.5.9 v2.5.11 v2.6.0 v2.6.1 v2.6.2 v2.6.3 v2.6.4 v2.6.5 v2.6.6"
 
 # RKE2
 for RKE2_VERSION in $RKE2_VERSIONS; do
