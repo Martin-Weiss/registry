fcb6444b067e87dad790e3d099e54bdbc0e08954
root <martin.weiss@suse.com>
2022-07-22 17:12:51 +0200

    fix sudo, other updates

--- harbor/get-images.sh
+++ harbor/get-images.sh
@@ -6,9 +6,9 @@ for IMAGE in $(cat harbor-images-v$HARBOR_VERSION.txt); do
 	echo $IMAGE
 	SHORTIMAGE=$(echo $IMAGE|awk -F '/' '{print $NF}')
 	IMAGE_FILENAME=$(echo $SHORTIMAGE.tar|sed s#:#X#g)
-	skopeo copy docker://$IMAGE docker-archive:harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
-#	docker pull $IMAGE
-#	docker save $IMAGE -o harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
+#	skopeo copy docker://$IMAGE docker-archive:harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
+	docker pull $IMAGE
+	docker save $IMAGE -o harbor-$HARBOR_CHART_VERSION/images/$IMAGE_FILENAME
 #	docker rmi $IMAGE
 done
 
--- harbor/install-k3s.sh
+++ harbor/install-k3s.sh
@@ -5,7 +5,7 @@ sudo cp k3s-$K3S_VERSION/k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/
 sudo cp k3s-$K3S_VERSION/k3s /usr/local/bin/k3s
 sudo chmod +x /usr/local/bin/k3s
 sudo chmod +x k3s-$K3S_VERSION/install.sh
-INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_SKIP_DOWNLOAD=true sudo k3s-$K3S_VERSION/install.sh
+sudo INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_SKIP_DOWNLOAD=true k3s-$K3S_VERSION/install.sh
 sudo systemctl daemon-reload
 sudo cp config.yaml /etc/rancher/k3s/config.yaml
 sudo cp registries.yaml /etc/rancher/k3s/registries.yaml
