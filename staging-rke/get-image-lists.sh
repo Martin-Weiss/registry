088edd29dbbb9329f84711c303a1525d55f3ebd5
root <martin.weiss@suse.com>
2023-02-03 12:05:44 +0100

    updating kubewarden, switching rancher to 2.7.1 and to prime

--- staging-rke/get-image-lists.sh
+++ staging-rke/get-image-lists.sh
@@ -3,9 +3,12 @@
 #wget -N https://github.com/mayflower/docker-ls/releases/download/v0.5.1/docker-ls-linux-amd64.zip
 
 #RKE2_VERSIONS="v1.19.7+rke2r1 v1.19.8+rke2r1 v1.19.9+rke2r1 v1.19.10+rke2r1 v1.19.11+rke2r1 v1.19.12+rke2r1 v1.19.13+rke2r1 v1.19.14+rke2r1 v1.19.16+rke2r1 v1.20.4+rke2r1 v1.20.5+rke2r1 v1.20.6+rke2r1 v1.20.7+rke2r1 v1.20.7+rke2r2 v1.20.8+rke2r1 v1.20.9+rke2r1 v1.20.10+rke2r1 v1.20.11+rke2r1 v1.20.11+rke2r2 v1.20.12+rke2r1 v1.20.13+rke2r1 v1.20.15+rke2r1 v1.21.2+rke2r1 v1.21.3+rke2r1 v1.21.4+rke2r3 v1.21.5+rke2r1 v1.21.6+rke2r1 v1.21.7+rke2r1 v1.21.7+rke2r2 v1.21.9+rke2r1 v1.21.10+rke2r1 v1.21.11+rke2r1 v1.22.8+rke2r1 v1.22.9+rke2r2 v1.22.10+rke2r2 v1.21.12+rke2r2"
-RKE2_VERSIONS="v1.22.13+rke2r1 v1.22.12+rke2r1 v1.23.9+rke2r1 v1.24.3+rke2r1 v1.24.4+rke2r1 v1.21.14+rke2r1 v1.23.10+rke2r1"
+#RKE2_VERSIONS="v1.22.13+rke2r1 v1.22.12+rke2r1 v1.23.9+rke2r1 v1.24.3+rke2r1 v1.24.4+rke2r1 v1.21.14+rke2r1 v1.23.10+rke2r1"
+RKE2_VERSIONS="v1.24.10+rke2r1 v1.25.6+rke2r1 v1.26.1+rke2r1"
+
 #RANCHER_VERSIONS="v2.5.5 v2.5.7 v2.5.8 v2.5.9 v2.5.11 v2.6.0 v2.6.1 v2.6.2 v2.6.3 v2.6.4 v2.6.5 v2.6.6 v2.6.7"
-RANCHER_VERSIONS="v2.6.8 v2.6.9 v2.7.0"
+#RANCHER_VERSIONS="v2.6.8 v2.6.9 v2.7.0"
+RANCHER_VERSIONS="v2.7.1"
 
 # RKE2
 for RKE2_VERSION in $RKE2_VERSIONS; do
@@ -14,5 +17,6 @@ done
 
 # Rancher
 for RANCHER_VERSION in $RANCHER_VERSIONS; do
-	wget -N "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt" -O rancher-images-$RANCHER_VERSION.txt; sed -i "s#^#docker.io/#g" rancher-images-$RANCHER_VERSION.txt
+	#wget -N "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt" -O rancher-images-$RANCHER_VERSION.txt; sed -i "s#^#docker.io/#g" rancher-images-$RANCHER_VERSION.txt
+	wget -N "https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt" -O rancher-images-$RANCHER_VERSION.txt; sed -i "s#^#registry.rancher.com/#g" rancher-images-$RANCHER_VERSION.txt
 done
